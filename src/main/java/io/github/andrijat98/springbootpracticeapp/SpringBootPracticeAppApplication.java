1d978669eaac2bf76118cad12a4d0c836f082da7
andrijat98 <andrijat98@gmail.com>
2022-12-24 11:36:17 +0100

    initial commit

---
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.andrijat98.springbootpracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrijat98.springbootpracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
