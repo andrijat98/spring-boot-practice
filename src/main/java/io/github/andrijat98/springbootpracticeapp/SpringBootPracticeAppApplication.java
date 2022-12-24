55afb724ed48bcd1dee8896613cadb2b03574f49
andrijat98 <andrijat98@gmail.com>
2022-12-24 12:19:04 +0100

    simeple restful api

--- src/main/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplication.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplication.java
@@ -2,6 +2,7 @@ package io.github.andrijat98.springbootpracticeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootPracticeAppApplication {
@@ -9,5 +10,11 @@ public class SpringBootPracticeAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticeAppApplication.class, args);
 	}
-
+	
+	@GetMapping
+	Customer getCustomer() {
+		return new Customer(1l, "Some name");
+	}
+	
+	public record Customer (Long id, String name) {}
 }
