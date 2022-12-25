c256a9e34507aae538fd6e76c2099e8b1cfab7b1
andrijat98 <andrijat98@gmail.com>
2022-12-25 17:38:55 +0100

    added customerService through dependency injection

--- src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
@@ -1,16 +1,25 @@
 package io.github.andrijat98.springbootpracticeapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.andrijat98.springbootpracticeapp.entities.Customer;
+import io.github.andrijat98.springbootpracticeapp.services.CustomerService;
 
 @RestController
 public class CustomerController {
 
+	private final CustomerService customerService;
+	
+	@Autowired //optional annotation
+	public CustomerController(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+	
 	@GetMapping("/")
 	Customer getCustomer() {
-		return new Customer(1l, "Some name");
+		return customerService.getCustomer();
 	}
 	
 }
--- src/main/java/io/github/andrijat98/springbootpracticeapp/services/CustomerService.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/services/CustomerService.java
@@ -1,5 +1,14 @@
 package io.github.andrijat98.springbootpracticeapp.services;
 
+import org.springframework.stereotype.Service;
+
+import io.github.andrijat98.springbootpracticeapp.entities.Customer;
+
+@Service
 public class CustomerService {
 
+	public Customer getCustomer() {
+		return new Customer(1l, "Some name");
+	}
+	
 }
