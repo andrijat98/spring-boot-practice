2aecd8c35be5272925f6a8a6901870343043556d
andrijat98 <andrijat98@gmail.com>
2022-12-26 16:57:39 +0100

    added REST mappings

--- src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
@@ -3,7 +3,11 @@ package io.github.andrijat98.springbootpracticeapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.andrijat98.springbootpracticeapp.entities.Customer;
@@ -24,4 +28,20 @@ public class CustomerController {
 		return customerService.getCustomers();
 	}
 	
+	@PostMapping
+	void createNewCustomer(Customer customer) {
+		System.out.println("Post request: ");
+		System.out.println(customer);
+	}
+	
+	@DeleteMapping(path = "{customerId}")
+	void delteCustomer(@PathVariable("customerId") Long id) {
+		System.out.println("Delete request for customer with id: " + id);
+	}
+	
+	@PutMapping
+	void updateCustomer(Customer customer) {
+		System.out.println("Put request: ");
+		System.out.println(customer);
+	}
 }
