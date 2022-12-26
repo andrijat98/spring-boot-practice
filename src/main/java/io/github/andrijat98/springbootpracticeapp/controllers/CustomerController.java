e537085e9bdf8b4c41a60d210805bffc790b59e4
andrijat98 <andrijat98@gmail.com>
2022-12-26 17:31:46 +0100

    added method for processing post mapping

--- src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.andrijat98.springbootpracticeapp.entities.Customer;
@@ -28,8 +29,8 @@ public class CustomerController {
 		return customerService.getCustomers();
 	}
 	
-	@PostMapping
-	void createNewCustomer(Customer customer) {
+	@PostMapping("/")
+	void createNewCustomer(@RequestBody Customer customer) {
 		System.out.println("Post request: ");
 		System.out.println(customer);
 	}
--- src/main/java/io/github/andrijat98/springbootpracticeapp/entities/Customer.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/entities/Customer.java
@@ -2,11 +2,13 @@ package io.github.andrijat98.springbootpracticeapp.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 public class Customer {
 	
 	private final Long id;
 	private final String name;
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private final String password;
 	
 	public Customer(Long id, String name, String password) {
