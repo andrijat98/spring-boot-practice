b6118cee3918b3d97cf80a03fffe48ca62d86a35
andrijat98 <andrijat98@gmail.com>
2023-01-12 15:39:05 +0100

    setup H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into customer (id, name, password, email) values (1, 'Some name 1', 'password1', 'mail1@gmail.com');
+insert into customer (id, name, password, email) values (2, 'Some name 2', 'password2', 'mail2@gmail.com');
+insert into customer (id, name, password, email) values (3, 'Some name 3', 'password3', 'mail3@gmail.com');
+insert into customer (id, name, password, email) values (4, 'Some name 4', 'password4', 'mail4@gmail.com');
+insert into customer (id, name, password, email) values (5, 'Some name 5', 'password5', 'mail5@gmail.com');
