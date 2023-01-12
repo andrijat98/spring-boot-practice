b59d68f98798ff973bd61a08db1031172d35edf8
andrijat98 <andrijat98@gmail.com>
2023-01-12 16:33:22 +0100

    added openFeign client

--- src/main/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplication.java
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/SpringBootPracticeAppApplication.java
@@ -2,8 +2,10 @@ package io.github.andrijat98.springbootpracticeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringBootPracticeAppApplication {
 
 	public static void main(String[] args) {
