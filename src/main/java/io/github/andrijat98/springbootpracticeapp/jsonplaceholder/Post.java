f86bb050a93961109eb43d31bb3e59404bd60e16
andrijat98 <andrijat98@gmail.com>
2023-01-12 16:42:13 +0100

    added mapping for feign client

---
+++ src/main/java/io/github/andrijat98/springbootpracticeapp/jsonplaceholder/Post.java
@@ -0,0 +1,41 @@
+package io.github.andrijat98.springbootpracticeapp.jsonplaceholder;
+
+public class Post {
+	
+	private final Integer userId;
+	private final Integer id;
+	private final String title;
+	private final String body;
+	
+	public Post(Integer userId, Integer id, String title, String body) {
+		super();
+		this.userId = userId;
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [userId=" + userId + ", id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
+	
+	
+	
+}
